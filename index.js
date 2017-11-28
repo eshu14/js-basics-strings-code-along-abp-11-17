1a559d64311a82fa068f09c4ca9c80fc369f7b45
Easwari Ramani <eshuprad@gmail.com>
2017-11-28 13:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-let currentUser="Eshu";
+let currentUser="Eshu ";
 let welcomeMessage="Welcome to Flatbook, !";
 let excitedWelcomeMessage="WELCOME TO FLATBOOK, !";
-let shortGreeting="Welcome, ";
+let shortGreeting="Welcome, !";
